7fc9152f5f67491539c76b009fbe2003633fa92a
ksonya <dylmaslove@gmail.com>
2016-07-18 11:08:35 +0600

    Показали картинку

--- app/src/main/java/com/example/sonya/coloringsun/MainActivity.java
+++ app/src/main/java/com/example/sonya/coloringsun/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.sonya.coloringsun;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView = (ImageView) findViewById(R.id.imageView);
+        try {
+            bitmap = BitmapFactory.decodeStream(getAssets().open("18789.jpg"));
+            imageView.setImageBitmap(bitmap);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-}
+
+    }
