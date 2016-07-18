dc7974abd4302b156deed9a998abde6751aaa258
ksonya <dylmaslove@gmail.com>
2016-07-18 15:02:30 +0600

    Добавили интерфейс

--- app/src/main/java/com/example/sonya/coloringsun/MainActivity.java
+++ app/src/main/java/com/example/sonya/coloringsun/MainActivity.java
@@ -2,30 +2,87 @@ package com.example.sonya.coloringsun;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView;
     private Bitmap bitmap;
+    int targetColor = Color.WHITE;
+    int newColor = Color.BLUE;
+    QueueLinearFloodFiller filler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         imageView = (ImageView) findViewById(R.id.imageView);
         try {
             bitmap = BitmapFactory.decodeStream(getAssets().open("18789.jpg"));
-            imageView.setImageBitmap(bitmap);
+            filler = new QueueLinearFloodFiller(bitmap,targetColor,newColor);
+            Point pt = new Point(1, 1);
+
+
+//            filler.floodFill(1, 1);
+
+           // FloodFill(bitmap, pt, Color.WHITE, Color.BLUE);
+
+            render();
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
+
+
+
 
     }
+
+    private void render() {
+        imageView.setImageBitmap(bitmap);
+
+    }
+
+    private void FloodFill(Bitmap bmp, Point pt, int targetColor, int replacementColor) {
+        Queue<Point> q = new LinkedList<Point>();
+        q.add(pt);
+        while (q.size() > 0) {
+            Point n = q.poll();
+            if (bmp.getPixel(n.x, n.y) != targetColor)
+                continue;
+
+            Point w = n, e = new Point(n.x + 1, n.y);
+            while ((w.x > 0) && (bmp.getPixel(w.x, w.y) == targetColor)) {
+                bmp.setPixel(w.x, w.y, replacementColor);
+                if ((w.y > 0) && (bmp.getPixel(w.x, w.y - 1) == targetColor))
+                    q.add(new Point(w.x, w.y - 1));
+                if ((w.y < bmp.getHeight() - 1)
+                        && (bmp.getPixel(w.x, w.y + 1) == targetColor))
+                    q.add(new Point(w.x, w.y + 1));
+                w.x--;
+            }
+            while ((e.x < bmp.getWidth() - 1)
+                    && (bmp.getPixel(e.x, e.y) == targetColor)) {
+                bmp.setPixel(e.x, e.y, replacementColor);
+
+                if ((e.y > 0) && (bmp.getPixel(e.x, e.y - 1) == targetColor))
+                    q.add(new Point(e.x, e.y - 1));
+                if ((e.y < bmp.getHeight() - 1)
+                        && (bmp.getPixel(e.x, e.y + 1) == targetColor))
+                    q.add(new Point(e.x, e.y + 1));
+                e.x++;
+            }
+        }
+    }
+
+}
